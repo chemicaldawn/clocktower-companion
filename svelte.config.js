6712735b3f03444563c6fb7ea8fb4ab232e46ab0
Dawn Schumacher <dawn@berkeley.edu>
2025-03-02 17:02:38 -0800

    Change basepath

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,11 @@ import adapter from "@sveltejs/adapter-static";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+        adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/clocktower-companion' : '',
+        }
+    }
 };
 
 export default config;
