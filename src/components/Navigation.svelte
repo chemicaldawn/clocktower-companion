345bfbb57b3c8a45403b4ee71ce09761405da42e
Dawn Schumacher <dawn@berkeley.edu>
2025-03-04 17:02:15 -0800

    Add more icon functionality, better mobile experience

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -31,11 +31,22 @@
         image : string
         imagealt : string
         position : string
+        small : boolean
 
-        constructor(idx : number, image : string, imagealt: string) {
+        class : string
+
+        constructor(idx : number, image : string, imagealt: string, small:boolean) {
             this.index = idx
             this.image = image
             this.imagealt = imagealt
+            this.small = small
+
+            if (small) {
+                this.class = "side-button"
+            } else {
+                this.class = "main-button"
+            }
+
             this.resetPosition()
         }
 
@@ -52,7 +63,7 @@
         }
 
         setPosition() {
-            let angle = ((this.index + 1) / (nav_items.length + 1)) * Math.PI
+            let angle = ((this.index) / (nav_items.length - 1)) * Math.PI
             this.position = `translate(${Math.cos(angle) * dist + offset}rem, ${-Math.sin(angle) * dist + offset}rem)`
             console.log(this.position)
         }
@@ -78,28 +89,32 @@
     }
 
     let nav_items = [
-        new NavItem(2, "setup.svg", "game setup"),
-        new NavItem(1, "notes.svg", "game notes"),
-        new NavItem(0, "settings.svg", "app settings")
+        new NavItem(4, "eye.svg", "show roles", true),
+        new NavItem(3, "setup.svg", "game setup", false),
+        new NavItem(2, "notes.svg", "game notes", false),
+        new NavItem(1, "settings.svg", "app settings", false),
+        new NavItem(0, "tag.svg", "show player names", true)
     ]
 </script>
 
 <main>
-    <div class="side-button">
-        <img alt="show roles" src="/images/icons/eye.svg">
+    <div id="help" class="meta-button">
+        <img alt="help" src="/images/icons/help.svg">
     </div>
-    <div id="navigation" role="button" tabindex="0" onkeydown={toggleExpansion} ontouchstart={toggleExpansion}>
-        <img src="/images/clock.svg" alt="clock">
-        <img src="/images/minutehand.svg" style:transform="rotate({minuteHandPos}deg) scale(1)" alt="clock">
-        <img src="/images/hourhand.svg" style:transform="rotate({hourHandPos}deg)" alt="clock">
-        {#each nav_items as item}
-            <div class="nav-item" role="button" style:transform={item.position} style:opacity="{opacity}">
-                <img alt={item.imagealt} src="/images/icons/{item.image}">
-            </div>
-        {/each}
+    <div id="central-nav">
+        <div id="navigation" role="button" tabindex="0" onkeydown={toggleExpansion} ontouchstart={toggleExpansion}>
+            <img src="/images/clock.svg" alt="clock">
+            <img src="/images/minutehand.svg" style:transform="rotate({minuteHandPos}deg) scale(1)" alt="clock">
+            <img src="/images/hourhand.svg" style:transform="rotate({hourHandPos}deg)" alt="clock">
+            {#each nav_items as item}
+                <div class={item.class} role="button" style:transform={item.position} style:opacity="{opacity}">
+                    <img alt={item.imagealt} src="/images/icons/{item.image}">
+                </div>
+            {/each}
+        </div>
     </div>
-    <div class="side-button">
-        <img alt="show names" src="/images/icons/tag.svg">
+    <div id="settings" class="meta-button">
+        <img alt="app settings" src="/images/icons/settings.svg">
     </div>
 </main>
 
@@ -107,23 +122,44 @@
 
     main {
         display: flex;
+        justify-content: space-between;
+        gap: 2.5rem;
+
+        width: 100%;
+    }
+
+    #central-nav {
+        display: flex;
         gap: 1.5rem;
     }
 
-    .side-button {
-        width: 2.5rem;
-        height: 2.5rem;
+    .meta-button {
+        pointer-events: all;
+
+        z-index: 1;
+        background-color: rgb(68, 18, 94);
         border-radius: 50%;
+        container-type: size;
 
-        margin: auto;
+        width: 3.25rem;
+        height: 3.25rem;
 
-        background-color: rgb(117, 53, 145);
+        margin-top: auto;
+        margin-bottom: auto;
 
         img {
             box-sizing: border-box;
-            margin: 20%;
-            width: 60%
+            margin: 17.5%;
+            width: 65%
         }
+    }
+
+    .meta-button#help {
+        background-color: rgb(53, 145, 124) !important;
+    }
+
+    .meta-button#settings {
+        background-color: rgb(73, 69, 70) !important;
     }
 
     #navigation {
@@ -150,7 +186,7 @@
             transition: transform 500ms ease-in-out;
         }
 
-        .nav-item {
+        .main-button {
             position: absolute;
             pointer-events: all;
 
@@ -171,7 +207,25 @@
                 margin: 17.5%;
                 width: 65%
             }
+        }
 
+        .side-button {
+            position: absolute;
+
+            width: 2.5rem;
+            min-height: 2.5rem;
+
+            border-radius: 50%;
+            margin: 0.375rem;
+
+            background-color: rgb(117, 53, 145);
+            transition: transform 150ms, opacity 180ms;
+
+            img {
+                box-sizing: border-box;
+                margin: 20%;
+                width: 60%
+            }
         }
     }
 </style>
